1b42c4edd52379462c0d8d2254616d2a6a4fe8bd
v2Kamikaze <victortorresfisica@gmail.com>
2023-07-03 04:57:59 -0300

    Funções úteis para WegbGl criadas

---
+++ src/gl_utils.ts
@@ -0,0 +1,86 @@
+export const GLUtils = {
+  InitGL: function (): WebGLRenderingContext {
+    const canvas = document.querySelector<HTMLCanvasElement>("#app")!;
+    const gl =
+      canvas.getContext("webgl") ||
+      (canvas.getContext("experimental-webgl") as WebGLRenderingContext);
+
+    if (gl === null) {
+      window.alert("Seu navegador não suporta WebGL");
+    }
+
+    return gl;
+  },
+
+  ClearCanvas: function (gl: WebGLRenderingContext) {
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  },
+
+  CreateProgram: function (
+    gl: WebGLRenderingContext,
+    ...shaders: WebGLShader[]
+  ) {
+    const program = gl.createProgram();
+
+    if (program === null) {
+      throw new Error("Program nulo");
+    } else {
+      shaders.forEach((shader) => {
+        gl.attachShader(program, shader);
+      });
+
+      gl.linkProgram(program);
+
+      const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+      if (!success) {
+        const errorLog = gl.getProgramInfoLog(program);
+        throw new Error(`Erro ao vincular programa: ${errorLog}`);
+      }
+    }
+
+    return program;
+  },
+
+  CreateVertexShader: function (gl: WebGLRenderingContext, shaderSrc: string) {
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+
+    if (vertexShader === null) {
+      throw new Error("VertexShader nulo");
+    } else {
+      gl.shaderSource(vertexShader, shaderSrc);
+      gl.compileShader(vertexShader);
+
+      const success = gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS);
+      if (!success) {
+        const errorLog = gl.getShaderInfoLog(vertexShader);
+        throw new Error(`Erro na compilação do Vertex shader: ${errorLog}`);
+      }
+    }
+
+    return vertexShader;
+  },
+
+  CreateFragmentShader: function (
+    gl: WebGLRenderingContext,
+    shaderSrc: string
+  ) {
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    if (fragmentShader === null) {
+      throw new Error("FragmentShader nulo");
+    } else {
+      gl.shaderSource(fragmentShader, shaderSrc);
+      gl.compileShader(fragmentShader);
+
+      const success = gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS);
+      if (!success) {
+        const errorLog = gl.getShaderInfoLog(fragmentShader);
+        throw new Error(`Erro na compilação do Fragment shader: ${errorLog}`);
+      }
+    }
+
+    return fragmentShader;
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,34 @@
+import { GLUtils } from "./gl_utils";
 
-const canvas = document.querySelector<HTMLCanvasElement>('#app')!;
-const ctx = canvas.getContext("2d")!
-ctx.rect(20, 20, 150, 100);
-ctx.fillStyle = "red";
-ctx.fill();
+const gl = GLUtils.InitGL();
+
+const vertexShaderSrc = document.getElementById("vertex-shader")!.innerText;
+const fragmentShaderSrc = document.getElementById("fragment-shader")!.innerText;
+
+const vertexShader = GLUtils.CreateVertexShader(gl, vertexShaderSrc);
+const fragmentShader = GLUtils.CreateFragmentShader(gl, fragmentShaderSrc);
+const program = GLUtils.CreateProgram(gl, vertexShader, fragmentShader);
+
+gl.useProgram(program);
+
+const triangle = Float32Array.from([0.0, 0.25, -0.25, -0.25, 0.25, -0.25]);
+
+const bufferPointer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, bufferPointer);
+gl.bufferData(gl.ARRAY_BUFFER, triangle, gl.STATIC_DRAW);
+const positionPointer = gl.getAttribLocation(program, "position");
+
+gl.enableVertexAttribArray(positionPointer);
+
+gl.vertexAttribPointer(
+  positionPointer,
+  2, // quantidade de dados em cada processamento
+  gl.FLOAT, // tipo de cada dado (tamanho)
+  false, // normalizar
+  2 * 4, // tamanho do bloco
+  0 // salto inicial
+);
+
+GLUtils.ClearCanvas(gl);
+
+gl.drawArrays(gl.TRIANGLES, 0, 3);
