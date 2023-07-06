b13987494c0c7d738310a86e7e6012eb3bbe580c
v2Kamikaze <victortorresfisica@gmail.com>
2023-07-06 03:01:36 -0300

    Texturas e transformação de rotação

--- src/gl_utils.ts
+++ src/gl_utils.ts
@@ -12,7 +12,48 @@ export const GLUtils = {
     return gl;
   },
 
-  CreateArrayBuffer: function (gl: WebGLRenderingContext, program: WebGLProgram, attrName: string, data: BufferSource): number {
+  LoadTextures: function (paths: string[]): Promise<HTMLImageElement[]> {
+    const promises = paths.map((path) => {
+      return new Promise<HTMLImageElement>((resolve, reject) => {
+        const texture = new Image();
+        texture.onload = () => resolve(texture);
+        texture.onerror = () =>
+          reject(new Error(`Failed to load texture: ${path}`));
+        texture.src = path;
+      });
+    });
+
+    return Promise.all(promises) as Promise<HTMLImageElement[]>;
+  },
+
+  LinkTexture: function (
+    gl: WebGLRenderingContext,
+    webglTextureObj: WebGLTexture,
+    textureTarget: number,
+    textureSrc: TexImageSource
+  ) {
+    gl.activeTexture(textureTarget);
+    gl.bindTexture(gl.TEXTURE_2D, webglTextureObj);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      0,
+      gl.RGBA,
+      gl.RGBA,
+      gl.UNSIGNED_BYTE,
+      textureSrc
+    );
+  },
+
+  CreateArrayBuffer: function (
+    gl: WebGLRenderingContext,
+    program: WebGLProgram,
+    attrName: string,
+    data: BufferSource
+  ): number {
     const bufferPointer = gl.createBuffer();
     if (!bufferPointer) {
       throw new Error("Não foi possível criar o buffer.");
@@ -29,7 +70,7 @@ export const GLUtils = {
     return positionPointer;
   },
 
-  ClearCanvas: function (gl: WebGLRenderingContext) {
+  SetAndClearCanvas: function (gl: WebGLRenderingContext) {
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,111 @@
 import { GLUtils } from "./gl_utils";
 
-const BLOCK_SIZE = 6 * 4;
+const BLOCK_SIZE = 4 * 4;
+var ANGLE = 0;
 
-const gl = GLUtils.InitGL();
+main();
 
-const vertexShaderSrc = document.getElementById("vertex-shader")!.innerText;
-const fragmentShaderSrc = document.getElementById("fragment-shader")!.innerText;
+async function main() {
+  const textures = await GLUtils.LoadTextures([
+    "../assets/gato.png",
+    "../assets/catioro.png",
+  ]);
 
-const vertexShader = GLUtils.CreateVertexShader(gl, vertexShaderSrc);
-const fragmentShader = GLUtils.CreateFragmentShader(gl, fragmentShaderSrc);
-const program = GLUtils.CreateProgram(gl, vertexShader, fragmentShader);
+  const gl = GLUtils.InitGL();
 
-gl.useProgram(program);
+  const vertexShaderSrc = document.getElementById("vertex-shader")!.innerText;
+  const fragmentShaderSrc =
+    document.getElementById("fragment-shader")!.innerText;
 
-// prettier-ignore
-const triangle = Float32Array.from([
-    0.0,  0.25, 1.0, 0.0, 0.0, 0.5,
-  -0.25, -0.25, 0.0, 1.0, 0.0, 0.8,
-   0.25, -0.25, 0.0, 0.0, 1.0, 0.2,
+  const vertexShader = GLUtils.CreateVertexShader(gl, vertexShaderSrc);
+  const fragmentShader = GLUtils.CreateFragmentShader(gl, fragmentShaderSrc);
+  const program = GLUtils.CreateProgram(gl, vertexShader, fragmentShader);
+
+  gl.useProgram(program);
+
+  // prettier-ignore
+  const triangle = Float32Array.from([
+  //  X     Y   TX   TY
+   -0.5,  0.5, 0.0, 0.0,
+   -0.5, -0.5, 0.0, 1.0,
+    0.5, -0.5, 1.0, 1.0,
+    0.5,  0.5, 1.0, 0.0,
+   -0.5,  0.5, 0.0, 0.0,
 ]);
 
+  const positionPointer = GLUtils.CreateArrayBuffer(
+    gl,
+    program,
+    "external_position",
+    triangle
+  );
 
-const positionPointer = GLUtils.CreateArrayBuffer(gl, program, "external_position", triangle);
+  gl.enableVertexAttribArray(positionPointer);
 
-gl.enableVertexAttribArray(positionPointer);
+  // prettier-ignore
+  gl.vertexAttribPointer(
+    positionPointer,
+    2,          // quantidade de dados em cada processamento
+    gl.FLOAT,   // tipo de cada dado (tamanho)
+    false,      // normalizar
+    BLOCK_SIZE, // tamanho do bloco
+    0           // salto inicial
+  );
 
-gl.vertexAttribPointer(
-  positionPointer,
-  2, // quantidade de dados em cada processamento
-  gl.FLOAT, // tipo de cada dado (tamanho)
-  false, // normalizar
-  BLOCK_SIZE, // tamanho do bloco
-  0 // salto inicial
-);
+  const textureCoordPointer = GLUtils.CreateArrayBuffer(
+    gl,
+    program,
+    "external_texture_coord",
+    triangle
+  );
+  gl.enableVertexAttribArray(textureCoordPointer);
 
-const colorPointer = GLUtils.CreateArrayBuffer(gl, program, "external_color", triangle);
-gl.enableVertexAttribArray(colorPointer);
+  // prettier-ignore
+  gl.vertexAttribPointer(
+    textureCoordPointer,
+    2,          // quantidade de dados em cada processamento
+    gl.FLOAT,   // tipo de cada dado (tamanho)
+    false,      // normalizar
+    BLOCK_SIZE, // tamanho do bloco
+    2 * 4       // salto inicial
+  );
 
-gl.vertexAttribPointer(
-  colorPointer,
-  4, // quantidade de dados em cada processamento
-  gl.FLOAT, // tipo de cada dado (tamanho)
-  false, // normalizar
-  BLOCK_SIZE, // tamanho do bloco
-  2 * 4 // salto inicial
-);
+  // Aplicando transaformação
+  const uTransform = gl.getUniformLocation(program, "external_transform");
+  gl.uniformMatrix4fv(uTransform, false, RotateMat(ANGLE));
 
+  // Criando e linkando as texturas
+  const uTextureSrcPointer = gl.getUniformLocation(program, "texture_src");
 
-GLUtils.ClearCanvas(gl);
+  const texture0 = gl.createTexture()!;
+  GLUtils.LinkTexture(gl, texture0, gl.TEXTURE0, textures[0]);
 
-gl.drawArrays(gl.TRIANGLES, 0, 3);
+  const texture1 = gl.createTexture()!;
+  GLUtils.LinkTexture(gl, texture1, gl.TEXTURE1, textures[1]);
 
+  GLUtils.SetAndClearCanvas(gl);
 
+  // Compartilhando vértices
+  gl.uniform1i(uTextureSrcPointer, 0);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 
+  gl.uniform1i(uTextureSrcPointer, 1);
+  gl.drawArrays(gl.TRIANGLES, 2, 3);
+
+  ANGLE += 10;
+  requestAnimationFrame(main);
+}
+
+function RotateMat(angle: number) {
+  const cos = Math.cos((angle * Math.PI) / 180);
+  const sin = Math.sin((angle * Math.PI) / 180);
+  // prettier-ignore
+  const ratationMat = [
+    cos, -sin, 0, 0,
+    sin,  cos, 0, 0,
+    0,    0,   1, 0,
+    0,    0,   0, 1,
+  ];
+
+  return ratationMat;
+}
